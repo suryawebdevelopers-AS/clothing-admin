2d5d2b9fab79a3b24d1e0bbb4f2a2bf4ccab27d8
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-03-20 15:11:34 +0530

    vercel destinated to ./index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,15 @@ import Navbar from "./components/Navbar/navbar";
 import Products from "./components/products/products";
 import ProductUpdate from "./components/products/productupdate";
 
-
 function App() {
   return (
     <BrowserRouter>
-      < Navbar/>
-    <Routes>
-        
-          <Route path="/inventory" element={< Inventory/>} />
-          <Route path="/products" element={<Products />} />
-          <Route path='/productupdate' element={<ProductUpdate/>}/>
-        
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/productupdate" element={<ProductUpdate />} />
+      </Routes>
     </BrowserRouter>
   );
 }
